9a3c83e0ca64e6128b24b2d6b683921e37dba557
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-02-19 23:02:29 +0100

    Refacto home page

---
+++ React/zartisan/src/components/ButtonSearchArtisanList/index.js
@@ -0,0 +1,30 @@
+/**
+ * Imports of dependencies
+ */
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from 'antd';
+import { withRouter } from 'react-router-dom';
+/**
+ * Local imports
+ */
+import './style.sass';
+import { postHomeSearch } from 'src/store/search/actions';
+
+const ButtonSearchArtisanList = withRouter(({ history, idJob, regionChange }) => {
+	const dispatch = useDispatch();
+	const handleSearch = () => {
+		dispatch(postHomeSearch(regionChange, idJob));
+		setTimeout(() => {
+			history.push('/liste-artisan');
+		}, 1000);
+	};
+
+	return (
+		<Button className="home-button-search" className="buttons" onClick={handleSearch}>
+			Recherche
+		</Button>
+	);
+});
+
+export default ButtonSearchArtisanList;
