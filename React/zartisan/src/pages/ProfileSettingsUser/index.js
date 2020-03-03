f5b7199df1b5e5fb923afba8e62f2476839aec73
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-03-03 18:12:05 +0100

    Fix: profile user session

--- React/zartisan/src/pages/ProfileSettingsUser/index.js
+++ React/zartisan/src/pages/ProfileSettingsUser/index.js
@@ -1,51 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Row } from 'antd';
-import 'antd/dist/antd.css';
-import FormEditUser from 'src/components/FormEditUser';
-import Loader from 'src/components/Loader';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Row } from "antd";
+import "antd/dist/antd.css";
+import FormEditUser from "src/components/FormEditUser";
+import Loader from "src/components/Loader";
 
 const ProfileSettingsUser = () => {
-	const userSelect = useSelector((state) => state.user);
+  const userSelect = useSelector(state => state.user);
 
-	console.log('ussser', userSelect);
+  if (userSelect != "") {
+    sessionStorage.setItem("ProfileUser", JSON.stringify(userSelect));
+  }
+  const sessionUser = JSON.parse(sessionStorage.getItem("ProfileUser"));
 
-	const [ profileUser, setProfileUser ] = useState({
-		pictureAvatar: '',
-		nickname: '',
-		firstname: '',
-		lastname: '',
-		phone: '',
-		mail: ''
-	});
+  const [profileUser, setProfileUser] = useState({
+    pictureAvatar: "",
+    nickname: "",
+    firstname: "",
+    lastname: "",
+    phone: "",
+    mail: ""
+  });
 
-	useEffect(
-		() => {
-			setProfileUser({
-				pictureAvatar: userSelect.picture,
-				nickname: userSelect.nickname,
-				firstname: userSelect.firstname,
-				lastname: userSelect.lastname,
-				phone: userSelect.phone,
-				mail: userSelect.email
-			});
-		},
-		[ userSelect ]
-	);
+  useEffect(() => {
+    if (sessionUser !== null) {
+      setProfileUser({
+        pictureAvatar: sessionUser.picture,
+        nickname: sessionUser.nickname,
+        firstname: sessionUser.firstname,
+        lastname: sessionUser.lastname,
+        phone: sessionUser.phone,
+        mail: sessionUser.email
+      });
+    }
+  }, [userSelect]);
 
-	//console.log(profileUser);
+  console.log("profile", profileUser);
 
-	return (
-		<div>
-			<Row type="flex" justify="space-around" align="middle">
-				{userSelect == '' ? (
-					<Loader />
-				) : (
-					<FormEditUser profileUser={profileUser} setProfileUser={setProfileUser} />
-				)}
-			</Row>
-		</div>
-	);
+  return (
+    <div>
+      <Row type="flex" justify="space-around" align="middle">
+        {sessionUser === null ? (
+          <Loader />
+        ) : (
+          <FormEditUser
+            profileUser={profileUser}
+            setProfileUser={setProfileUser}
+          />
+        )}
+      </Row>
+    </div>
+  );
 };
 
 export default ProfileSettingsUser;
