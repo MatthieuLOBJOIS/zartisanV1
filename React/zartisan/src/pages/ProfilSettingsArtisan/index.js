4b9111b40ff68281941dd1b79bad34033e5f68c1
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-05-23 14:47:56 +0200

    Add localStorage for profile artisan

--- React/zartisan/src/pages/ProfilSettingsArtisan/index.js
+++ React/zartisan/src/pages/ProfilSettingsArtisan/index.js
@@ -1,82 +1,90 @@
 //Imports of dependencies
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Row } from 'antd';
-import 'antd/dist/antd.css';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Row } from "antd";
+import "antd/dist/antd.css";
 
 //Local imports
-import { useLoading } from 'src/hooks/useLoading';
-import './style.sass';
+import { useLoading } from "src/hooks/useLoading";
+import "./style.sass";
 
 //Components
-import FormEditArtisan from 'src/components/FormEditArtisan';
-import Loader from 'src/components/Loader';
+import FormEditArtisan from "src/components/FormEditArtisan";
+import Loader from "src/components/Loader";
 
 //Components content of page profile artisan
 const ProfilSettingsArtisan = () => {
-	//Hooks
-	let toLoading = useLoading();
-	const artisanSelector = useSelector((state) => state.artisan);
+  //Hooks
+  let toLoading = useLoading();
+  const artisanSelector = useSelector(state => state.artisan);
 
-	const [ profileArtisan, setProfileArtisan ] = useState({
-		firstname: '',
-		lastname: '',
-		siret: '',
-		company: '',
-		numberWay: '',
-		way: '',
-		postalCode: '',
-		city: '',
-		description: '',
-		pictureAvatar: '',
-		pictureGalery: '',
-		phone: '',
-		email: ''
-	});
+  const [sessionArtisan, setSessionArtisan] = useState(null);
+  const [profileArtisan, setProfileArtisan] = useState({
+    firstname: "",
+    lastname: "",
+    siret: "",
+    company: "",
+    numberWay: "",
+    way: "",
+    postalCode: "",
+    city: "",
+    description: "",
+    pictureAvatar: "",
+    pictureGalery: "",
+    phone: "",
+    email: ""
+  });
 
-	//Creat session for account artisan
-	if (artisanSelector !== '') {
-		sessionStorage.setItem('ProfileArtisan', JSON.stringify(artisanSelector[0]));
-	}
-	const sessionArtisan = JSON.parse(sessionStorage.getItem('ProfileArtisan'));
-	const artisanObject = sessionArtisan;
+  //Creat session for account artisan
 
-	useEffect(
-		() => {
-			setProfileArtisan({
-				firstname: artisanObject.firstname,
-				lastname: artisanObject.lastname,
-				siret: artisanObject.siret,
-				company: artisanObject.company,
-				numberWay: artisanObject.numberWay,
-				way: artisanObject.way,
-				postalCode: artisanObject.postalCode,
-				city: artisanObject.city,
-				description: artisanObject.companyDescription,
-				pictureAvatar: artisanObject.picture,
-				pictureGalery: artisanObject.pictureFolder,
-				phone: artisanObject.phone,
-				email: artisanObject.email
-			});
-		},
-		[ artisanSelector ]
-	);
+  //const sessionArtisan = JSON.parse(localStorage.getItem("ProfileArtisan"));
 
-	return (
-		<div>
-			<Row type="flex" justify="space-around" align="middle">
-				{profileArtisan && toLoading === false ? (
-					<FormEditArtisan
-						artisanObject={artisanObject}
-						profileArtisan={profileArtisan}
-						setProfileArtisan={setProfileArtisan}
-					/>
-				) : (
-					<Loader />
-				)}
-			</Row>
-		</div>
-	);
+  useEffect(
+    () => {
+      setSessionArtisan(JSON.parse(localStorage.getItem("ProfileArtisan")));
+    },
+    [artisanSelector]
+  );
+
+  useEffect(
+    () => {
+      console.log(sessionArtisan);
+      if (sessionArtisan !== null) {
+        setProfileArtisan({
+          firstname: sessionArtisan.firstname,
+          lastname: sessionArtisan.lastname,
+          siret: sessionArtisan.siret,
+          company: sessionArtisan.company,
+          numberWay: sessionArtisan.numberWay,
+          way: sessionArtisan.way,
+          postalCode: sessionArtisan.postalCode,
+          city: sessionArtisan.city,
+          description: sessionArtisan.companyDescription,
+          pictureAvatar: sessionArtisan.picture,
+          pictureGalery: sessionArtisan.pictureFolder,
+          phone: sessionArtisan.phone,
+          email: sessionArtisan.email
+        });
+      }
+    },
+    [sessionArtisan]
+  );
+
+  return (
+    <div>
+      <Row type="flex" justify="space-around" align="middle">
+        {sessionArtisan !== null && toLoading === false ? (
+          <FormEditArtisan
+            artisanObject={sessionArtisan}
+            profileArtisan={profileArtisan}
+            setProfileArtisan={setProfileArtisan}
+          />
+        ) : (
+          <Loader />
+        )}
+      </Row>
+    </div>
+  );
 };
 
 export default ProfilSettingsArtisan;
--- React/zartisan/src/store/artisan/middleware.js
+++ React/zartisan/src/store/artisan/middleware.js
@@ -1,65 +1,70 @@
 //Imports of dependencies
-import cookies from 'js-cookie';
-import axios from 'axios';
+import cookies from "js-cookie";
+import axios from "axios";
 
 //Local imports
-import { ARTISAN_DATA } from 'src/store/artisan/actions';
-import { ARTISAN_EDIT } from 'src/store/artisan/actions';
-import { artisanInfo } from 'src/store/artisan/actions';
-import { NAME_SERVER } from 'src/store/register/actions';
+import { ARTISAN_DATA } from "src/store/artisan/actions";
+import { ARTISAN_EDIT } from "src/store/artisan/actions";
+import { artisanInfo } from "src/store/artisan/actions";
+import { NAME_SERVER } from "src/store/register/actions";
 
-export default (store) => (next) => (action) => {
-	switch (action.type) {
-		case ARTISAN_DATA: {
-			let token = cookies.get('TOKEN');
-			return axios({
-				method: 'post',
-				url: `${NAME_SERVER}/v1/artisan/single`,
-				data: {
-					email: action.email
-				},
-				headers: { Authorization: `Bearer ${token}` }
-			})
-				.then((response) => {
-					if (response.status === 200) {
-						//Creat session for page-artisan
-						localStorage.setItem('PageArtisan', JSON.stringify(response.data));
-						store.dispatch(artisanInfo(response.data));
-					}
-				})
-				.catch(function(error) {
-					// handle error
-					//console.log(error);
-				})
-				.finally(function() {
-					// always executed
-				});
-		}
+export default store => next => action => {
+  switch (action.type) {
+    case ARTISAN_DATA: {
+      let token = cookies.get("TOKEN");
+      return axios({
+        method: "post",
+        url: `${NAME_SERVER}/v1/artisan/single`,
+        data: {
+          email: action.email
+        },
+        headers: { Authorization: `Bearer ${token}` }
+      })
+        .then(response => {
+          if (response.status === 200) {
+            //Creat session for page-artisan
+            localStorage.setItem("PageArtisan", JSON.stringify(response.data));
 
-		case ARTISAN_EDIT: {
-			let token = cookies.get('TOKEN');
-			return axios({
-				method: 'post',
-				url: `${NAME_SERVER}/api/v1/artisan/edit`,
-				data: {
-					email: action.email,
-					companyDescription: action.description,
-					picture: action.pictureAvatar,
-					phone: action.phone,
-					pictureFolder: action.pictureGalery
-				},
-				headers: { Authorization: `Bearer ${token}` }
-			})
-				.then((response) => {})
-				.catch(function(error) {
-					// handle error
-					//console.log(error);
-				})
-				.finally(function() {
-					// always executed
-				});
-		}
-	}
+            localStorage.setItem(
+              "ProfileArtisan",
+              JSON.stringify(response.data[0])
+            );
+            store.dispatch(artisanInfo(response.data));
+          }
+        })
+        .catch(function(error) {
+          // handle error
+          //console.log(error);
+        })
+        .finally(function() {
+          // always executed
+        });
+    }
 
-	next(action);
+    case ARTISAN_EDIT: {
+      let token = cookies.get("TOKEN");
+      return axios({
+        method: "post",
+        url: `${NAME_SERVER}/api/v1/artisan/edit`,
+        data: {
+          email: action.email,
+          companyDescription: action.description,
+          picture: action.pictureAvatar,
+          phone: action.phone,
+          pictureFolder: action.pictureGalery
+        },
+        headers: { Authorization: `Bearer ${token}` }
+      })
+        .then(response => {})
+        .catch(function(error) {
+          // handle error
+          //console.log(error);
+        })
+        .finally(function() {
+          // always executed
+        });
+    }
+  }
+
+  next(action);
 };
