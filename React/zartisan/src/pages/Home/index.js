d970319df6b80f5ff0f4bd4bda58d6b370acf971
MatthieuLOBJOIS <lobjois.mt@gmail.com>
2020-05-25 21:47:11 +0200

    Add dynamic document title

--- React/zartisan/src/pages/Home/index.js
+++ React/zartisan/src/pages/Home/index.js
@@ -1,85 +1,112 @@
 //Imports of dependencies
-import React, { useState, useEffect } from 'react';
-import { Row } from 'antd';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { Row } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { Helmet } from "react-helmet";
 
 //Local imports
-import './style.sass';
+import "./style.sass";
 
 //Components
-import FranceMap from 'src/components/FranceMap';
-import ButtonSearchArtisanList from 'src/components/ButtonSearchArtisanList';
-import ButtonJob from 'src/components/ButtonJob';
-import ButtonRegion from 'src/components/ButtonRegion';
-import Loader from 'src/components/Loader';
+import FranceMap from "src/components/FranceMap";
+import ButtonSearchArtisanList from "src/components/ButtonSearchArtisanList";
+import ButtonJob from "src/components/ButtonJob";
+import ButtonRegion from "src/components/ButtonRegion";
+import Loader from "src/components/Loader";
 
-import { getRegions } from 'src/store/regions/actions';
+import { getRegions } from "src/store/regions/actions";
 
 //Components content of page home
 const Home = () => {
-	//Hooks
-	const dispatch = useDispatch();
-	const [ visibleButtonJobs, setvisibleButtonJobs ] = useState(false);
-	const [ regionChange, setRegion ] = useState('Choisissez une Région');
-	const [ jobChange, setJobChange ] = useState('Choisissez votre métier');
-	const [ idJob, setIdJob ] = useState('');
-	const [ regionArray, setRegionArray ] = useState([]);
+  //Hooks
+  const dispatch = useDispatch();
+  const [visibleButtonJobs, setvisibleButtonJobs] = useState(false);
+  const [regionChange, setRegion] = useState("Choisissez une Région");
+  const [jobChange, setJobChange] = useState("Choisissez votre métier");
+  const [idJob, setIdJob] = useState("");
+  const [regionArray, setRegionArray] = useState([]);
 
-	useEffect(() => {
-		dispatch(getRegions());
-	}, []);
+  useEffect(() => {
+    dispatch(getRegions());
+  }, []);
 
-	const regions = useSelector((state) => state.regions);
+  const regions = useSelector(state => state.regions);
 
-	useEffect(
-		() => {
-			setRegionArray(regions);
-		},
-		[ regions ]
-	);
+  useEffect(
+    () => {
+      setRegionArray(regions);
+    },
+    [regions]
+  );
 
-	return (
-		<div style={{ width: '100vw', textAlign: 'center' }}>
-			{regionArray.length !== 0 ? (
-				<div className="home">
-					<Row className="home-content-button" type="flex" justify="space-around" align="middle">
-						<Row type="flex" className="home-button-dropdown" justify="space-around" align="middle">
-							<ButtonRegion
-								regionChange={regionChange}
-								setRegion={setRegion}
-								visibleButtonJobs={visibleButtonJobs}
-								setvisibleButtonJobs={setvisibleButtonJobs}
-							/>
-						</Row>
+  return (
+    <div style={{ width: "100vw", textAlign: "center" }}>
+      {regionArray.length !== 0 ? (
+        <div className="home">
+          <Helmet>
+            <title>Z'Artisan - Accueil</title>
+          </Helmet>
+          <Row
+            className="home-content-button"
+            type="flex"
+            justify="space-around"
+            align="middle"
+          >
+            <Row
+              type="flex"
+              className="home-button-dropdown"
+              justify="space-around"
+              align="middle"
+            >
+              <ButtonRegion
+                regionChange={regionChange}
+                setRegion={setRegion}
+                visibleButtonJobs={visibleButtonJobs}
+                setvisibleButtonJobs={setvisibleButtonJobs}
+              />
+            </Row>
 
-						<Row type="flex" justify="space-around" align="middle" className="home-button-dropdown">
-							<ButtonJob
-								jobChange={jobChange}
-								setJobChange={setJobChange}
-								setIdJob={setIdJob}
-								visibleButtonJobs={visibleButtonJobs}
-								setvisibleButtonJobs={setvisibleButtonJobs}
-							/>
-						</Row>
-					</Row>
+            <Row
+              type="flex"
+              justify="space-around"
+              align="middle"
+              className="home-button-dropdown"
+            >
+              <ButtonJob
+                jobChange={jobChange}
+                setJobChange={setJobChange}
+                setIdJob={setIdJob}
+                visibleButtonJobs={visibleButtonJobs}
+                setvisibleButtonJobs={setvisibleButtonJobs}
+              />
+            </Row>
+          </Row>
 
-					<Row type="flex" justify="space-around" align="middle" className="home-france">
-						<FranceMap
-							setvisibleButtonJobs={setvisibleButtonJobs}
-							setRegion={setRegion}
-							regionChange={regionChange}
-						/>
-					</Row>
+          <Row
+            type="flex"
+            justify="space-around"
+            align="middle"
+            className="home-france"
+          >
+            <FranceMap
+              setvisibleButtonJobs={setvisibleButtonJobs}
+              setRegion={setRegion}
+              regionChange={regionChange}
+            />
+          </Row>
 
-					<Row type="flex" justify="space-around" align="middle">
-						<ButtonSearchArtisanList regionChange={regionChange} idJob={idJob} />
-					</Row>
-				</div>
-			) : (
-				<Loader />
-			)}
-		</div>
-	);
+          <Row type="flex" justify="space-around" align="middle">
+            <ButtonSearchArtisanList
+              regionChange={regionChange}
+              idJob={idJob}
+            />
+          </Row>
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- React/zartisan/src/pages/ListArtisan/index.js
+++ React/zartisan/src/pages/ListArtisan/index.js
@@ -1,140 +1,150 @@
 //Imports of dependencies
-import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import 'antd/dist/antd.css';
-import { Row, List, Rate } from 'antd';
-import slugify from 'slugify';
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import "antd/dist/antd.css";
+import { Row, List, Rate } from "antd";
+import slugify from "slugify";
+import { Helmet } from "react-helmet";
 
 //Local imports
-import { NAME_SERVER } from 'src/store/register/actions';
-import { artisanData } from 'src/store/artisan/actions';
-import { useLoading } from 'src/hooks/useLoading';
-import './style.sass';
+import { NAME_SERVER } from "src/store/register/actions";
+import { artisanData } from "src/store/artisan/actions";
+import { useLoading } from "src/hooks/useLoading";
+import "./style.sass";
 
 //Components
-import ButtonSearchArtisanList from 'src/components/ButtonSearchArtisanList';
-import ButtonJob from '../../components/ButtonJob';
-import ButtonRegion from '../../components/ButtonRegion';
-import Loader from 'src/components/Loader';
+import ButtonSearchArtisanList from "src/components/ButtonSearchArtisanList";
+import ButtonJob from "../../components/ButtonJob";
+import ButtonRegion from "../../components/ButtonRegion";
+import Loader from "src/components/Loader";
 
 //Components content of page list-artisan
 const ListArtisan = () => {
-	//Hooks
-	let toLoading = useLoading();
-	const dispatch = useDispatch();
-	const arrayArtisan = useSelector((state) => state.search);
+  //Hooks
+  let toLoading = useLoading();
+  const dispatch = useDispatch();
+  const arrayArtisan = useSelector(state => state.search);
 
-	const [ visibleButtonJobs, setvisibleButtonJobs ] = useState(false);
-	const [ regionChange, setRegion ] = useState('Choisissez une Région');
-	const [ jobChange, setJobChange ] = useState('Choisissez votre métier');
-	const [ idJob, setIdJob ] = useState('');
-	const [ star, setStar ] = useState(1);
-	const [ sessionData, setSessionData ] = useState(null);
-	useEffect(
-		() => {
-			setSessionData(JSON.parse(localStorage.getItem('ListArtisan')));
-		},
-		[ arrayArtisan ]
-	);
+  const [visibleButtonJobs, setvisibleButtonJobs] = useState(false);
+  const [regionChange, setRegion] = useState("Choisissez une Région");
+  const [jobChange, setJobChange] = useState("Choisissez votre métier");
+  const [idJob, setIdJob] = useState("");
+  const [star, setStar] = useState(1);
+  const [sessionData, setSessionData] = useState(null);
+  useEffect(
+    () => {
+      setSessionData(JSON.parse(localStorage.getItem("ListArtisan")));
+    },
+    [arrayArtisan]
+  );
 
-	let objectArtisan = '';
+  let objectArtisan = "";
 
-	const listData = [];
+  const listData = [];
 
-	//Loop the different artisan of the search
-	if (sessionData != null) {
-		sessionData.map((obj) => {
-			if (obj.companyDescription == null) {
-				obj.companyDescription = '';
-			}
-			objectArtisan = obj;
-			listData.push(objectArtisan);
-		});
-	}
+  //Loop the different artisan of the search
+  if (sessionData != null) {
+    sessionData.map(obj => {
+      if (obj.companyDescription == null) {
+        obj.companyDescription = "";
+      }
+      objectArtisan = obj;
+      listData.push(objectArtisan);
+    });
+  }
 
-	useEffect(
-		() => {
-			setStar(objectArtisan.averageRate);
-		},
-		[ objectArtisan.averageRate ]
-	);
+  useEffect(
+    () => {
+      setStar(objectArtisan.averageRate);
+    },
+    [objectArtisan.averageRate]
+  );
 
-	const RateStar = () => {
-		return <Rate style={{ fontSize: '1em' }} disabled defaultValue={star} />;
-	};
+  const RateStar = () => {
+    return <Rate style={{ fontSize: "1em" }} disabled defaultValue={star} />;
+  };
 
-	//Redirect to page-artisan
-	const LinkArtisan = withRouter(({ history, item }) => {
-		const handleSearch = () => {
-			dispatch(artisanData(item.id, item.email));
+  //Redirect to page-artisan
+  const LinkArtisan = withRouter(({ history, item }) => {
+    const handleSearch = () => {
+      dispatch(artisanData(item.id, item.email));
 
-			history.push(
-				`/page-artisan/${slugify(item.company, {
-					lower: true
-				})}`
-			);
-		};
-		return <a onClick={handleSearch}>{item.company}</a>;
-	});
+      history.push(
+        `/page-artisan/${slugify(item.company, {
+          lower: true
+        })}`
+      );
+    };
+    return <a onClick={handleSearch}>{item.company}</a>;
+  });
 
-	return (
-		<div>
-			<Row type="flex" justify="space-around" align="middle">
-				{toLoading === false ? (
-					<div className="list-artisan-content">
-						<div align="middle">
-							<ButtonRegion
-								regionChange={regionChange}
-								setRegion={setRegion}
-								visibleButtonJobs={visibleButtonJobs}
-								setvisibleButtonJobs={setvisibleButtonJobs}
-							/>
+  return (
+    <div>
+      <Helmet>
+        <title>Z'Artisan - Liste d'artisans</title>
+      </Helmet>
+      <Row type="flex" justify="space-around" align="middle">
+        {toLoading === false ? (
+          <div className="list-artisan-content">
+            <div align="middle">
+              <ButtonRegion
+                regionChange={regionChange}
+                setRegion={setRegion}
+                visibleButtonJobs={visibleButtonJobs}
+                setvisibleButtonJobs={setvisibleButtonJobs}
+              />
 
-							<ButtonJob
-								jobChange={jobChange}
-								setJobChange={setJobChange}
-								setIdJob={setIdJob}
-								visibleButtonJobs={visibleButtonJobs}
-								setvisibleButtonJobs={setvisibleButtonJobs}
-							/>
+              <ButtonJob
+                jobChange={jobChange}
+                setJobChange={setJobChange}
+                setIdJob={setIdJob}
+                visibleButtonJobs={visibleButtonJobs}
+                setvisibleButtonJobs={setvisibleButtonJobs}
+              />
 
-							<div className="bloc-button-search-artisan">
-								<ButtonSearchArtisanList regionChange={regionChange} idJob={idJob} />
-							</div>
-						</div>
+              <div className="bloc-button-search-artisan">
+                <ButtonSearchArtisanList
+                  regionChange={regionChange}
+                  idJob={idJob}
+                />
+              </div>
+            </div>
 
-						<List
-							itemLayout="horizontal"
-							size="small"
-							pagination={{
-								onChange: (page) => {},
-								pageSize: 5
-							}}
-							grid={{
-								gutter: 16
-							}}
-							dataSource={listData}
-							renderItem={(item) => (
-								<List.Item className="antListItem">
-									<List.Item.Meta
-										className="ant-list-item "
-										avatar={
-											<img style={{ width: '60px' }} src={`${NAME_SERVER}/${item.picture}`} />
-										}
-										title={<LinkArtisan item={item} />}
-										description={item.companyDescription}
-									/>
-									<RateStar />
-								</List.Item>
-							)}
-						/>
-					</div>
-				) : (
-					<Loader />
-				)}
-			</Row>
-		</div>
-	);
+            <List
+              itemLayout="horizontal"
+              size="small"
+              pagination={{
+                onChange: page => {},
+                pageSize: 5
+              }}
+              grid={{
+                gutter: 16
+              }}
+              dataSource={listData}
+              renderItem={item => (
+                <List.Item className="antListItem">
+                  <List.Item.Meta
+                    className="ant-list-item "
+                    avatar={
+                      <img
+                        style={{ width: "60px" }}
+                        src={`${NAME_SERVER}/${item.picture}`}
+                      />
+                    }
+                    title={<LinkArtisan item={item} />}
+                    description={item.companyDescription}
+                  />
+                  <RateStar />
+                </List.Item>
+              )}
+            />
+          </div>
+        ) : (
+          <Loader />
+        )}
+      </Row>
+    </div>
+  );
 };
 export default ListArtisan;
